263c6ff45d99a1dcb31830b3f2d1a3068b7431e3
oksana-1 <4438488@gmail.com>
2021-04-28 19:44:15 +0300

    Add clickHandler

--- src/components/ToDoItem/ToDoItem.js
+++ src/components/ToDoItem/ToDoItem.js
@@ -2,12 +2,22 @@ import React, { Component } from "react";
 import "./ToDoItem.css";
 
 export default class ToDoItem extends Component{
+	onLabelClick = () => {
+		console.log(this.props.todo.label);
+	}
 	render() {
 		const { label, important} = this.props.todo;
 		return (
-			<span className={ important ? "redText" : "" }>
-			{label}
-		</span>
+			<span>
+				<span
+					className={ important ? "redText" : "" }
+					onClick={this.onLabelClick}
+				>
+					{label}
+				</span>
+				<button>DEL</button>
+				<button>!</button>
+			</span>
 		);
 	}
 }
